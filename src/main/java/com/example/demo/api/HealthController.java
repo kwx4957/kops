7373d4500bb689b41e3c82e5688e7e0284e795be
dansam <33277725+kwx4957@users.noreply.github.com>
2024-03-07 23:14:50 +0900

    add yaml configure and dockerfile

---
+++ src/main/java/com/example/demo/api/HealthController.java
@@ -0,0 +1,14 @@
+package com.example.demo.api;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController {
+
+    @GetMapping("/hello")
+    public String hello(){
+        return "Hello world!";
+    }
+}
